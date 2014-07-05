6e3f25a0a2fadd77873d204e1f9debe5ffd339b3
Jeff Taggart <jeff@jetaggart.com>
2014-07-05 16:00:14 -0600

    Get site running

--- Import.hs
+++ Import.hs
@@ -16,15 +16,5 @@ import           Settings.Development as Import
 import           Settings.StaticFiles as Import
 import           SharedTypes          as Import
 
-#if __GLASGOW_HASKELL__ >= 704
-import           Data.Monoid          as Import
-                                                 (Monoid (mappend, mempty, mconcat),
+import           Data.Monoid          as Import (Monoid (mappend, mempty, mconcat),
                                                  (<>))
-#else
-import           Data.Monoid          as Import
-                                                 (Monoid (mappend, mempty, mconcat))
-
-infixr 5 <>
-(<>) :: Monoid m => m -> m -> m
-(<>) = mappend
-#endif
--- Model.hs
+++ Model.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Model where
 
 import Yesod
 import Data.Text (Text)
 import Database.Persist.Quasi
 import Data.Typeable (Typeable)
+import Control.Applicative
+import Control.Monad
+import Prelude (($))
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
@@ -11,3 +15,25 @@ import Data.Typeable (Typeable)
 -- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlOnlySettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
+
+instance ToJSON (Entity Post) where
+    toJSON (Entity pid p) = object
+        [ "id"      .= (String $ toPathPiece pid)
+        , "title"   .= postTitle p
+        , "content" .= postContent p
+        ]
+
+instance FromJSON Post where
+    parseJSON (Object o) = Post
+        <$> o .: "title"
+        <*> o .: "content"
+
+    parseJSON _ = mzero
+
+-- { "id": 1, "post_id": 1, "content": "The comment content" }
+instance ToJSON (Entity Comment) where
+    toJSON (Entity cid c) = object
+        [ "id"      .= (String $ toPathPiece cid)
+        , "post_id" .= (String $ toPathPiece $ commentPost c)
+        , "content" .= commentContent c
+        ]
